a3688a566b8a311a5bb4e5c9a44def0bb3d64dff
Will Tang <willwyatttang@gmail.com>
2016-02-29 23:15:29 -0800

    Have a working version of stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,54 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //non JSON values
+  var type = Object.prototype.toString.call(obj);
+  var output = [];
+
+  function putInQuotes(string) {
+  	return '"' + string + '"';
+  }
+
+  switch(type) {
+
+  	case '[object Null]':
+  		return "null";
+
+  	case '[object String]':
+  		return putInQuotes(obj);
+
+  	case '[object Boolean]':
+  		return String(obj);
+
+  	case '[object Number]':
+  		return String(obj);
+
+  	case '[object Array]':
+			var len = obj.length;
+
+	  	for (var i = 0; i < len; i++) {
+	  		output[i] = stringifyJSON(obj[i]) || "null";
+	  	}
+	  	return "[" + output.join(',') + "]"
+
+  	case '[object Object]': 
+	  	
+	  	for (var prop in obj) {
+	  		if (obj.hasOwnProperty(prop)) {
+		  		var value = stringifyJSON(obj[prop]);
+
+		  		if(value) {
+			  		var pair = putInQuotes(prop) + ":" + stringifyJSON(obj[prop]);
+			  		output.push(pair);
+		  		}
+		  	}
+	  	}
+	  	
+	  	return "{" + output.join(",") + "}";
+	  
+	  default: 
+	  	return undefined;
+	}
+
 };
+
